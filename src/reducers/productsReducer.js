6fcaf559b9a32504a63965eb4e8f2b015ecc4120
maru <larrosa.marilina@gmail.com>
2020-06-18 17:20:21 -0300

    Adding react redux

---
+++ src/reducers/productsReducer.js
@@ -0,0 +1,11 @@
+// each reducer has its own state
+
+const initialState = {
+    products: [],
+    error: null,
+    loading: false,
+}
+
+export default function(state = initialState, action) {
+    
+}
