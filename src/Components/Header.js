3d4cc074b2cb59e5cb3e98e34bb5ee4fdb935fb1
maruslarrosa <larrosa.marilina@gmail.com>
2020-09-11 18:57:03 -0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="container">
         <h1>
           <Link to={"/"} className="text-light">
-            CRUD - React, Redux, REST API & Axios
+            CRUD - React, Redux, REST API & Axios hola
           </Link>
         </h1>
       </div>
